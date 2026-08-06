41b2522e62a155c39ce5612074aa6c0dfb8a0a74
RajmaniZ <mr.raj725sharma@gmail.com>
2026-08-07 05:17:42 +0530

    Add CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import ScrollToTop from "./components/stopScroll/ScrollToTop";
-
-// import Navbar from "./components/Navbar/Navbar";
-// import Footer from "./components/Footer/Footer";
-
-// /* Pages */
-// import Home from "./components/pages/Home/Home";
-// import About from "./components/pages/About/About";
-// import Google from "./components/pages/Google/Google";
-// import GetInTouch from "./components/pages/GetInTouch/GetInTouch";
-// import Contact from "./components/pages/Contact/Contact";
-// import Career from "./components/pages/Career/Career";
-
-// /* Products */
-// // import Birthday from "./components/pages/Birthday/Birthday";
-// import Birthday from "./components/pages/Birthday/Birthday";
-// import Anniversary from "./components/pages/Anniversary/Anniversary";
-// import BabyCeremony from "./components/pages/BabyCeremony/BabyCeremony";
-// import KidsSpecial from "./components/pages/KidsSpecial/KidsSpecial";
-// import HaldiMehndi from "./components/pages/HaldiMehndi/HaldiMehndi";
-
-// /* Cart & Payment */
-// import Cart from "./components/pages/Cart/Cart";
-// import Payment from "./components/pages/Payment/Payment";
-// import PaymentSuccess from "./components/pages/PaymentSuccess/PaymentSuccess";
-// import History from "./components/pages/History/History";
-
-// import VerifyPayment from "./components/pages/PaymentVerify/PaymentVerify";
-// // import VerifyPayment from "./components/pages/VerifyPayment/VerifyPayment";
-// /* Legal */
-// import Terms from "./components/pages/Terms/Terms";
-// import Privacy from "./components/pages/Privacy/Privacy";
-// import Sitemap from "./components/pages/Sitemap/Sitemap";
-// import Disclaimer from "./components/pages/Disclaimer/Disclaimer";
-// import RefundPolicy from "./components/pages/RefundPolicy/RefundPolicy";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <ScrollToTop/>
-//         <Navbar />
-
-//         <Routes>
-
-//           {/* Home */}
-//           <Route path="/" element={<Home />} />
-
-//           {/* General */}
-//           <Route path="/about" element={<About />} />
-//           <Route path="/google" element={<Google />} />
-//           <Route path="/get-in-touch" element={<GetInTouch />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/career" element={<Career />} />
-
-//           {/* Products */}
-//           <Route path="/birthday" element={<Birthday />} />
-//           <Route path="/anniversary" element={<Anniversary />} />
-//           <Route path="/baby-ceremony" element={<BabyCeremony />} />
-//           <Route path="/kids-special" element={<KidsSpecial />} />
-//           <Route path="/haldi-mehndi" element={<HaldiMehndi />} />
-
-//           {/* Cart */}
-//           <Route path="/cart" element={<Cart />} />
-
-//           {/* Payment */}
-//           <Route path="/payment" element={<Payment />} />
-
-//           <Route
-//             path="/payment/success/:invoiceId"
-//             element={<PaymentSuccess />}
-//           />
-
-//           <Route
-//             path="/history"
-//             element={<History />}
-//           />
-
-//           <Route
-//             path="/verify/:token"
-//             element={<VerifyPayment />}
-//           />
-
-//           {/* Legal */}
-//           <Route path="/terms" element={<Terms />} />
-//           <Route path="/privacy-policy" element={<Privacy />} />
-//           <Route path="/sitemap" element={<Sitemap />} />
-//           <Route path="/disclaimer" element={<Disclaimer />} />
-//           <Route path="/refund-policy" element={<RefundPolicy />} />
-
-//           {/* 404 */}
-//           <Route
-//             path="*"
-//             element={
-//               <div
-//                 style={{
-//                   minHeight: "70vh",
-//                   display: "flex",
-//                   justifyContent: "center",
-//                   alignItems: "center",
-//                   fontSize: "2rem",
-//                   fontWeight: "600",
-//                 }}
-//               >
-//                 404 | Page Not Found
-//               </div>
-//             }
-//           />
-
-//         </Routes>
-
-//         <Footer />
-//       {/* </ScrollToTop> */}
-
-
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
 
 import {
@@ -129,9 +8,9 @@ import {
   useLocation,
 } from "react-router-dom";
 
-import ScrollToTop from "./components/stopScroll/ScrollToTop";
+import ScrollToTop from "./components/StopScroll/ScrollToTop";
 
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/navbar";
 import Footer from "./components/Footer/Footer";
 
 /* Pages */
